f1b8993f2769927fc9225e06ecbc6555986e2877
JoaoGabrielTN <joaogt008@gmail.com>
2024-04-03 22:58:12 -0300

    docstrings e comentários md

--- neurônio MCP/MCP.py
+++ neurônio MCP/MCP.py
@@ -4,5 +4,12 @@ class MCP:
         pass
 
     def run(X, w, theta):
+        """Return a numpy array with the predictions from the MCP neuron.
+
+        Keyword arguments:
+        X -- a matrix containing the feature vectors 
+        w -- the parameters vector
+        theta -- the threshold for the MCP
+        """
         return np.where(w.T.dot(X)-theta <= 0, 0, 1)
         
