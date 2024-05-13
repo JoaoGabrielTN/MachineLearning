3c3b198e5e1d402d9c57cea3e9b6b02f8bc166db
JoaoGabrilTN <joaogt008@gmail.com>
2024-05-12 22:59:50 -0300

    perceptron convergence completed

--- neuronio_mcp/MCP.py
+++ neuronio_mcp/MCP.py
@@ -16,16 +16,34 @@ class MCP:
         w = np.c_[np.zeros(w.shape[0]) + theta, w]
         return np.where(w.dot(X.T) <= 0, 0, 1)
     
-    def fit(self, data, labels):
+    def fit(self, data, labels, iterations=100):
         data = np.c_[np.ones(data.shape[0]).T, data].T
         self.w = np.random.uniform(size=data.shape[0]).reshape(-1, 1)
         z = np.where(self.w.T.dot(data) <= 0, 0, 1)
-        print(data.shape)
-        print(self.w.shape)
+        print(self.w)
 
+        count = 0
+        convergence = False
+
+        while not convergence:
+            convergence = True
+            for i in range(len(labels)):
+                if z[:, i] - labels[i]:
+                    self.w = self.w - (z[:, i] - labels[i])*data[:, i].reshape(-1, 1)
+                    z = np.where(self.w.T.dot(data) <= 0, 0, 1)
+                    convergence = False
+                
+            count = count + 1
+            if count == iterations:
+                print('iterations: ', count)
+                break
+
+        print(count)
+    #def predict(data, labels):
 
 perceptron = MCP()
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 X_labels = [1, 1, 1, 0]
 perceptron.fit(X, X_labels)
-        
+
+
